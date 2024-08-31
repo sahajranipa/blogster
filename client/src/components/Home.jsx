7be73fa1c2567532924fabce54905985079e4047
sahajranipa <sahajranipa@gmail.com>
2024-08-31 11:52:43 +0530

    did some changes in theming

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -6,16 +6,16 @@ import Button from "./ui/Button";
 const Home = () => {
   const navigate = useNavigate();
   return (
-    <main>
+    <main className="mt-[10%]">
       <div className="font-headingFont text-center">
         <h1 className="font-bold text-center text-9xl">Blogster.</h1>
-        <p className="text-center text-3xl mt-8 w-3/4 mx-auto">
+        <p className=" text-3xl text-balance leading-[3rem] mt-8 w-3/4 mx-auto">
           Your one and only platform for blogging.Where you will create your
           gradient for knowledge.Which will lead you to the milestones🚩
         </p>
 
         <Button
-          className="bg-black text-white font-headingFont font-medium rounded-full p-3 mt-10 "
+          className="bg-black text-white font-headingFont font-medium rounded-full px-5 py-3 mt-10 "
           onClick={() => navigate("/sign-up")}>
           Start Your Journey <BsArrowRight className="inline w-7 h-7" />
         </Button>
--- client/src/components/Profile.jsx
+++ client/src/components/Profile.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Profile = () => {
-  return <div>Profile</div>;
+  return <form></form>;
 };
 
 export default Profile;
